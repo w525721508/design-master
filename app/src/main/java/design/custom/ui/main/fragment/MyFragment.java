d4ce351dc14d347fa82554559ae0b852b0c96343
wanghai <wanghai@teddymobile.cn>
2019-03-12 18:54:25 +0800

    增加我的页面逻辑

--- app/src/main/java/design/custom/ui/main/fragment/MyFragment.java
+++ app/src/main/java/design/custom/ui/main/fragment/MyFragment.java
@@ -1,10 +1,15 @@
 package design.custom.ui.main.fragment;
 
 
+import android.content.Intent;
+import android.view.View;
+
 import design.custom.R;
-import design.root.base.base.BaseFragment;
-import design.custom.ui.main.MainPresenter;
 import design.custom.databinding.FragmentMyBinding;
+import design.custom.ui.main.MainPresenter;
+import design.root.base.base.BaseFragment;
+import design.root.base.util.Global;
+import design.root.base.util.OnClickListener;
 
 /**
  * Created by Administrator on 2018/1/26.
@@ -19,5 +24,26 @@ public class MyFragment extends BaseFragment<MainPresenter, FragmentMyBinding> {
     @Override
     public void initView() {
         mViewBinding.ic.tvTitle.setText("我的");
+        mViewBinding.tvMyMobile.setText(Global.userEntity.getMobile());
+        mViewBinding.tvMySex.setText(Global.userEntity.getSex());
+        mViewBinding.tvMyUserName.setText(Global.userEntity.getUsername());
+        mViewBinding.setOnClickListener(new OnClickListener() {
+            @Override
+            protected void myOnClickListener(View v) {
+                switch (v.getId()) {
+                    case R.id.btn_close: {
+                        //修改密码
+                        getActivity().finish();
+                    }
+                    break;
+                    case R.id.btnMyLogout: {
+                        //退出
+                        getActivity().finish();
+                        getActivity().startActivity(new Intent(getContext(), design.root.base.ui.sign.LoginActivity.class));
+                    }
+                    break;
+                }
+            }
+        });
     }
 }
